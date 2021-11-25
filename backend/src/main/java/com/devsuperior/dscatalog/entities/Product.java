842ffcd56e03954f623224c46b16dc8d08d6776a
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-25 02:53:40 -0300

    Product repository, service, resource, GET ok

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -41,7 +41,7 @@ public class Product implements Serializable {
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	Set<Category> categories = new HashSet<>();
 
-	Product() {
+	public Product() {
 
 	}
 
