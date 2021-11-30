f07834c4c234b03aa5b10032ba8224e2e823bdac
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-30 10:47:15 -0300

    Ajust of Junit, and pom.xml

--- backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DscatalogApplicationTests {
+public class DscatalogApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
-
 }
