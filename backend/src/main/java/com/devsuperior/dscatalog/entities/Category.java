314cd2153ac8cd84cf4a21c50fae8cbfe5eacfef
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-01-11 07:39:56 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -32,6 +35,8 @@ public class Category implements Serializable{
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -58,8 +63,6 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	
 	
 	public Instant getCreatedAt() {
 		return createdAt;
@@ -80,6 +83,12 @@ public class Category implements Serializable{
 		updatedAt = Instant.now();
 	}
 	
+	
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
